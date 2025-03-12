fb97e873bc79dcc2a8fd253bdb0ce9b26406d95f
ciaranpender <72555471+ciaranpender@users.noreply.github.com>
2025-03-12 10:24:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,36 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "main" {
-   name = "TestTF"
-   location = "North Europe"
+resource "azurerm_resource_group" "AZDevops" {
+  name     = "AZDevops"
+  location = "West Europe"
 }
 
+resource "azurerm_network_security_group" "AZDevopsNSG" {
+  name                = "AZDevopsNSG"
+  location            = "North Europe"
+  resource_group_name = "AZDevops"
+}
+
+resource "azurerm_virtual_network" "AZDevopsVnet" {
+  name                = "AZDevopsVnet"
+  location            = "North Europe"
+  resource_group_name = "AZDevops"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.AZDevopsNSG.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
